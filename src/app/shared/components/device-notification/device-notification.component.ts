6b890d29567721f4465e40beab85c50eb49eea57
keephacking <savigms2@gamil.com>
2019-05-12 04:48:53 +0530

    alarm added and made some fixes

--- src/app/shared/components/device-notification/device-notification.component.ts
+++ src/app/shared/components/device-notification/device-notification.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input, OnChanges, SimpleChanges } from "@angular/cor
 import { BehaviorSubject } from "rxjs";
 import { LocationService } from "../location/location.service";
 import { ThemeService } from "../../services/theme.service";
+import { distinctUntilChanged, tap, filter } from "rxjs/operators";
+import { AudioService } from "../../services/audio.service";
 
 @Component({
   selector: "app-device-notification",
@@ -9,16 +11,35 @@ import { ThemeService } from "../../services/theme.service";
   styleUrls: ["./device-notification.component.scss"]
 })
 export class DeviceNotificationComponent implements OnInit, OnChanges {
-  @Input()
-  data;
+  @Input("res")
+  deviceResult;
 
+  @Input() boundary;
+  alarmKey: string = "alarm";
   boundary$: BehaviorSubject<boolean> = new BehaviorSubject(null);
 
-  constructor(private mapService: LocationService, private themeService: ThemeService) {}
+  constructor(private mapService: LocationService, private themeService: ThemeService, private audioService: AudioService) {}
 
-  ngOnChanges(changes: SimpleChanges): void {}
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes["deviceResult"] && changes["deviceResult"].currentValue) {
+      this.checkBoundary(changes["deviceResult"].currentValue);
+    }
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.audioService.preload(this.alarmKey, "/assets/alarm.mp3");
+    this.boundary$
+      .pipe(
+        filter(x=>!this.audioService.isMuted$.value),
+        tap(x => {
+          if (x != null) {
+            if (x) this.audioService.stop(this.alarmKey);
+            else this.audioService.play(this.alarmKey);
+          }
+        })
+      )
+      .subscribe();
+  }
 
   // credits to user:69083 for this specific function
   arePointsNear(checkPoint, centerPoint, km) {
@@ -28,27 +49,26 @@ export class DeviceNotificationComponent implements OnInit, OnChanges {
     var dy = Math.abs(centerPoint.lat - checkPoint.lat) * ky;
     return Math.sqrt(dx * dx + dy * dy) <= km;
   }
-  checkBoundary(device: any) {
-    if (device.Boundary) {
-      if (device.Result.lattitude && device.Result.longitude) {
-        device.isOk = this.arePointsNear(
-          { lat: +device.Result.lattitude, lng: +device.Result.longitude },
-          JSON.parse(device.Boundary.latLng),
-          device.Boundary.radius / 1000
+  checkBoundary(result: any) {
+    if (this.boundary) {
+      if (result.lattitude && result.longitude) {
+        let res = this.arePointsNear(
+          { lat: +result.lattitude, lng: +result.longitude },
+          JSON.parse(this.boundary.latLng),
+          this.boundary.radius / 1000
         );
-        this.boundary$.next(device.isOk);
+        this.boundary$.next(res);
       }
     } else {
       this.boundary$.next(null);
     }
   }
 
-  onTracking(device: any) {
-    console.log(device);
-    if (device.Result && device.Result.lattitude && device.Result.longitude) {
+  onTracking() {
+    if (this.deviceResult && this.deviceResult.lattitude && this.deviceResult.longitude) {
       let latLng = {
-        lat: +device.Result.lattitude,
-        lng: +device.Result.longitude
+        lat: +this.deviceResult.lattitude,
+        lng: +this.deviceResult.longitude
       };
       this.mapService.openModal({ enableSelection: false, marker: latLng });
     } else {
